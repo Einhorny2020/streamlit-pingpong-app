845d031e21fd3c5101132884cae8db4a8e2a4e2a
Einhorny2020 <78635435+Einhorny2020@users.noreply.github.com>
2025-03-29 17:54:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(page_title="🏓 Jeu Ping-Pong IA", layout="centered")
-st.title("🏓 Jeu de Ping-Pong – Version Intégrée")
+st.set_page_config(page_title="Jeu Ping-Pong", layout="centered")
+st.title("Jeu de Ping-Pong – Version Intégrée")
 
-st.markdown("### 🕹️ Utilisez les flèches ← → pour contrôler la raquette.")
+st.markdown("### Utilisez les flèches ← → pour contrôler la raquette.")
 st.markdown("**Objectif** : Garder la balle en jeu le plus longtemps possible.")
 
 with open("game.html", "r") as f:
